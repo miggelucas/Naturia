a721b03f20bf85a98709a0d0cc2ac8810ade1f0b
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-03-23 17:14:29 -0300

    first commit

---
+++ Naturia/NaturiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaturiaApp.swift
+//  Naturia
+//
+//  Created by Lucas Migge de Barros on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NaturiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
