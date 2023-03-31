2c2dd82ee2d5279e5a861546a69a42b3fd289664
Lucas <lucasrsvalenca@gmail.com>
2023-03-30 23:04:30 -0300

    add scaleWidth and scaleHeight methods

--- Naturia/UI/Responsive.swift
+++ Naturia/UI/Responsive.swift
@@ -9,7 +9,11 @@ import Foundation
 import SwiftUI
 
 class Responsive {
-    static func scale(s: Double) -> Double {
-        return UIScreen.main.bounds.size.width * s / 834
+    static func scaleWidth(s: Double) -> Double {
+        return UIScreen.main.bounds.size.width * s / 1194
+    }
+    
+    static func scaleHeight(s: Double) -> Double {
+        return UIScreen.main.bounds.size.height * s / 834
     }
 }
