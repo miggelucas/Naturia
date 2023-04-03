138e5384d8943d7d4bbe8e8fc58dfa4614f2910e
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-04-03 14:57:57 -0300

    Creating files and image assets

---
+++ Naturia/UI/Modules/ObservativeRepository/CardObservative.swift
@@ -0,0 +1,21 @@
+//
+//  CardObservative.swift
+//  Naturia
+//
+//  Created by Lucas Migge de Barros on 03/04/23.
+//
+
+import SwiftUI
+
+struct CardObservative: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardObservative_Previews: PreviewProvider {
+    static var previews: some View {
+        CardObservative()
+    }
+}
---
+++ Naturia/UI/Modules/ObservativeRepository/ObservatoryRepositoryView.swift
@@ -0,0 +1,20 @@
+//
+//  ObservatoryRepositoryView.swift
+//  Naturia
+//
+//  Created by Lucas Migge de Barros on 03/04/23.
+//
+
+import SwiftUI
+
+struct ObservatoryRepositoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ObservatoryRepositoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        ObservatoryRepositoryView()
+    }
+}
