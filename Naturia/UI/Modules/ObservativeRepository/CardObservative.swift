6fad8bdc39cae546c1387611b3005e118c860012
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-04-03 19:24:23 -0300

    basic view for catalogo

--- Naturia/UI/Modules/ObservativeRepository/CardObservative.swift
+++ Naturia/UI/Modules/ObservativeRepository/CardObservative.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
-struct CardObservative: View {
+struct CardObservative: View, Hashable {
+    
+    let plantIconDrawn: Image
+    let plantName: String
+    
+    
+    // Implementação do protocolo Hashable
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(plantName)
+    }
+    
+    static func == (lhs: CardObservative, rhs: CardObservative) -> Bool {
+        lhs.plantName == rhs.plantName
+    }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("Card Observative")
+            
+            HStack {
+                plantIconDrawn
+                
+                Spacer()
+                
+                Text(plantName)
+                    .font(.system(size: 25))
+                    .multilineTextAlignment(.center)
+            }
+            .padding(.trailing, 23.0)
+            .padding(.leading, 16.0)
+        }
+        .frame(width: 270, height: 134)
     }
 }
 
 struct CardObservative_Previews: PreviewProvider {
     static var previews: some View {
-        CardObservative()
+        CardObservative(plantIconDrawn: Image("Green Plant"), plantName: "Espada de São Jorge")
     }
 }
--- Naturia/UI/Modules/ObservativeRepository/ObservatoryRepositoryView.swift
+++ Naturia/UI/Modules/ObservativeRepository/ObservatoryRepositoryView.swift
@@ -8,10 +8,69 @@
 import SwiftUI
 
 struct ObservatoryRepositoryView: View {
+    
+    let elements = [
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Verde"),
+        
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Vermelha44"),
+        
+        CardObservative(plantIconDrawn: Image("Violet Plant"), plantName: "Planta Vermelha213"),
+        
+        CardObservative(plantIconDrawn: Image("Green Plant"), plantName: "Planta Vermelha1"),
+        
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Vermelha2"),
+        
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Vermelha3"),
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Vermelha4"),
+        
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Vermelha5"),
+        
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Vermelha6"),
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Vermelha7"),
+        
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Vermelha8"),
+        
+        CardObservative(plantIconDrawn: Image("Red Plant"), plantName: "Planta Vermelha9")
+    ]
+    
+    
+    let gridLayout = [GridItem(.flexible()),
+                      GridItem(.flexible()),
+                      GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Botão de voltar")
+                
+                Spacer()
+            }
+            .padding(.bottom, 4)
+            
+            VStack(spacing: 16) {
+                Text("Jornada Observativa")
+                
+                Text("Escolha alguma das plantas abaixo e vá ao mundo para desenhá-la")
+            }
+            
+            .padding(.bottom, 48)
+
+            
+            ScrollView {
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 32) {
+                    ForEach(elements, id: \.self) { element in
+                        element
+                    }
+                    
+                }
+                
+
+            }
+        }
     }
 }
+
+
 
 struct ObservatoryRepositoryView_Previews: PreviewProvider {
     static var previews: some View {
