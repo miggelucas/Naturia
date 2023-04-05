6e767c234abd8813adc5ea0e4064379f1f67e367
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-04-05 17:04:59 -0300

    backGroundView

---
+++ Naturia/UI/Components/BackgroundView.swift
@@ -0,0 +1,30 @@
+//
+//  BackgroundView.swift
+//  Naturia
+//
+//  Created by Lucas Migge de Barros on 05/04/23.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        ZStack {
+            
+            LinearGradient(colors: [ Color("background-dark"),
+                                     Color("background-light")],
+                           startPoint: .bottomLeading,
+                           endPoint: .topTrailing)
+            Image("texturaBackground")
+                .resizable()
+                .opacity(0.7)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView()
+    }
+}
