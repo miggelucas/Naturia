bf7076369270407b44262815930743453bda810a
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-04-06 00:07:11 -0300

    feature(18): revertendo alterações da contentView

--- Naturia/UI/ContentView.swift
+++ Naturia/UI/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        NavigationStack {
-//            appView
-//        }
-        GaleryView()
+        NavigationStack {
+            appView
+        }
     }
     
     private var appView: some View {
